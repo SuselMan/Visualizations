5100fac92072d29e1ca30a215a1167d316da6bfa
Ilya Pavlukhin <i.pavluhin@ya.ru>
2025-11-01 19:41:22 +0300

    build: missed changes

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
+export default defineConfig({
+    base: '/Visualizations/',
+    plugins: [react(), svgr()],
+    resolve: {
+        alias: {
+            '@': path.resolve(path.dirname(fileURLToPath(import.meta.url)), 'src'),
+            'ui-kit': path.resolve(path.dirname(fileURLToPath(import.meta.url)), 'external/ui-kit'),
+        },
+    },
+});
